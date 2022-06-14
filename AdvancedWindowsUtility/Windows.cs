23c2489f8e6628037f76983179d138844b3177ed
Omer Huseyin GUL <68775313+omerhuseyingul@users.noreply.github.com>
2022-06-14 23:03:40 +0300

    Add files via upload

---
+++ AdvancedWindowsUtility/Windows.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedWindowsUtility
+{
+    public class Windows
+    {
+        /// <summary>
+        /// This synchronous function returns the amount of memory available based on the package parameter you provide.
+        /// </summary>
+        /// <param name="type">Packet Type</param>
+        /// <returns>availableSpace(decimal)</returns>
+        /// <exception cref="WindowsServiceException"></exception>
+        public static decimal GetAvailableMemorySpace(string type)
+        {
+            try
+            {
+                decimal availableSpace = default;
+                ManagementObjectSearcher searcher =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT * FROM Win32_PerfFormattedData_PerfOS_Memory");
+                    
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    if (type == "GBytes") 
+                        availableSpace = Convert.ToDecimal(queryObj[$"AvailableMBytes"]) / 1024;
+
+                    else
+                        availableSpace = Convert.ToDecimal(queryObj[$"Available{type}"]);
+                }
+
+                if (type == "GBytes")              
+                    return Decimal.Round(availableSpace, 2);
+
+                else 
+                    return availableSpace;
+
+            }
+            catch (ManagementException e)
+            {
+                throw new WindowsServiceException($"A problem was encountered with the mathematical operations or the {type} parameter you entered.");
+            }
+        }
+
+        /// <summary>
+        /// This synchronous function returns whether the application name you have given is open in transactions.
+        /// </summary>
+        /// <param name="procParam">Target Process Parameter</param>
+        /// <returns>true or false</returns>
+        /// <exception cref="WindowsServiceException"></exception>
+        public static bool IsRunningProcess(string procParam)
+        {
+            try
+            {
+                bool isRunningProcess = default;
+                ManagementObjectSearcher searcher =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT * FROM Win32_Process");
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    if (Equals(queryObj["Name"], procParam))
+                    {
+                        isRunningProcess = true;
+                        break;
+                    }
+                }
+
+                return isRunningProcess;
+            }
+            catch (ManagementException e)
+            {
+                throw new WindowsServiceException($"An error occurred while searching for a {procParam} process parameter.");
+            }
+        }
+
+
+        /// <summary>
+        /// This synchronous function returns the registered user information via Win32_OS.
+        /// </summary>
+        /// <returns>example@domain.com</returns>
+        /// <exception cref="WindowsServiceException"></exception>
+        public static string GetRegisteredMail()
+        {
+            try
+            {
+                string registeredMail = default;
+                ManagementObjectSearcher searcher =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT * FROM Win32_OperatingSystem");
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                    registeredMail = queryObj["RegisteredUser"].ToString();
+
+                return registeredMail;
+            }
+            catch (ManagementException e)
+            {
+                throw new WindowsServiceException("An error occurred while searching for a registered user.");
+            }
+        }
+
+        /// <summary>
+        /// This synchronous function that returns the full name of your operating system from string type using WMI (Win32API).
+        /// </summary>
+        /// <returns>Operating System Full Name (string)</returns>
+        /// <exception cref="WindowsServiceException"></exception>
+        public static string GetOperatingSystemName()
+        {
+            try
+            {
+                string operatingSystemName = default;
+                ManagementObjectSearcher searcher =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                     "SELECT * FROM Win32_OperatingSystem");
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                    operatingSystemName = Convert.ToString(queryObj["Caption"]);
+
+                return operatingSystemName;
+            }
+            catch (Exception)
+            {
+                throw new WindowsServiceException("An error occurred while searching for a Operating System name.");
+            }
+
+        }
+
+        /// <summary>
+        /// It is a synchronous function that returns the user name of your device from string type using the Environment class.
+        /// </summary>
+        /// <returns>Microsoft.Windows.Username (string)</returns>
+        /// <exception cref="WindowsServiceException"></exception>
+        public static string GetUsername()
+        {
+            try
+            {
+                return Environment.UserName;
+            }
+            catch (Exception)
+            {
+                throw new WindowsServiceException("An error occurred while searching for a Username.");
+            }
+        }
+
+        /// <summary>
+        /// This asynchronous function returns the amount of memory available based on the package parameter you provide.
+        /// </summary>
+        /// <param name="type">Packet Type</param>
+        /// <returns>availableSpace(decimal)</returns>
+        /// <exception cref="WindowsServiceException"></exception>
+        public static Task<decimal> GetAvailableMemorySpaceAsync(string type)
+        {
+            try
+            {
+                decimal availableSpace = default;
+                ManagementObjectSearcher searcher =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT * FROM Win32_PerfFormattedData_PerfOS_Memory");
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    if (type == "GBytes")
+                        availableSpace = Convert.ToDecimal(queryObj[$"AvailableMBytes"]) / 1024;
+
+                    else
+                        availableSpace = Convert.ToDecimal(queryObj[$"Available{type}"]);
+                }
+
+                if (type == "GBytes")
+                    return Task.FromResult<decimal>(Decimal.Round(availableSpace, 2));
+
+                else
+                    return Task.FromResult<decimal>(availableSpace);
+
+            }
+            catch (ManagementException e)
+            {
+                throw new WindowsServiceException($"A problem was encountered with the mathematical operations or the {type} parameter you entered.");
+            }
+        }
+
+        /// <summary>
+        /// This asynchronous function returns whether the application name you have given is open in transactions.
+        /// </summary>
+        /// <param name="procParam">Target Process Parameter</param>
+        /// <returns>true or false</returns>
+        /// <exception cref="WindowsServiceException"></exception>
+        public static Task<bool> IsRunningProcessAsync(string procParam)
+        {
+            try
+            {
+                bool isRunningProcess = default;
+                ManagementObjectSearcher searcher =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT * FROM Win32_Process");
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    if (Equals(queryObj["Name"], procParam))
+                    {
+                        isRunningProcess = true;
+                        break;
+                    }
+                }
+
+                return Task.FromResult<bool>(isRunningProcess);
+            }
+            catch (ManagementException e)
+            {
+                throw new WindowsServiceException($"An error occurred while searching for a {procParam} process parameter.");
+            }
+        }
+
+        /// <summary>
+        /// This asynchronous function returns the registered user information via Win32_OS.
+        /// </summary>
+        /// <returns>example@domain.com</returns>
+        /// <exception cref="WindowsServiceException"></exception>
+        public static Task<string> GetRegisteredMailAsync()
+        {
+            try
+            {
+                string registeredMail = default;
+                ManagementObjectSearcher searcher =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT * FROM Win32_OperatingSystem");
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                    registeredMail = queryObj["RegisteredUser"].ToString();
+
+                return Task.FromResult<string>(registeredMail);
+            }
+            catch (ManagementException e)
+            {
+                throw new WindowsServiceException("An error occurred while searching for a registered user.");
+            }
+        }
+
+        /// <summary>
+        /// It is a asynchronous function that returns the full name of your operating system from string type using WMI (Win32API).
+        /// </summary>
+        /// <returns>Operating System Full Name (string)</returns>
+        /// <exception cref="WindowsServiceException"></exception>
+        public static Task<string> GetOperatingSystemNameAsync()
+        {
+            try
+            {
+                var name = (from x in new ManagementObjectSearcher("SELECT Caption FROM Win32_OperatingSystem").Get().Cast<ManagementObject>()
+                            select x.GetPropertyValue("Caption")).FirstOrDefault();
+                return Task.FromResult<string>(name!= null ? name.ToString() : "Unknown");
+            }
+            catch (Exception)
+            {
+                throw new WindowsServiceException("An error occurred while searching for a Operating System name.");
+            }
+        }
+
+        /// <summary>
+        /// It is a asynchronous function that returns the user name of your device from string type using the Environment class.
+        /// </summary>
+        /// <returns>Microsoft.Windows.Username (string)</returns>
+        /// <exception cref="WindowsServiceException"></exception>
+        public static Task<string> GetUsernameAsync()
+        {
+            return Task.FromResult<string>(Environment.UserName);
+        }
+    }
+
+    public class WindowsServiceException : Exception
+    {
+        public WindowsServiceException() { }
+        public WindowsServiceException(object message) : base(message.ToString()) { }
+    } 
+}
--- README.md
+++ README.md
@@ -1,2 +1,214 @@
-# OnePlatforms.AWU
+
+![Logo](https://cdn.discordapp.com/attachments/986282378341265509/986283143524925550/banner.png)
+
+
+# AWU (Advanced Windows Utility)
+
 One Platforms Advanced Windows Utility, makes it easier to use Win32 API and WMI resources.
+
+<div align='center' text-algin='center'>
+<img src="https://img.shields.io/github/forks/One-Platforms/OnePlatforms.AWU">
+<img src="https://img.shields.io/github/stars/One-Platforms/OnePlatforms.AWU">
+<img src="https://img.shields.io/github/license/One-Platforms/OnePlatforms.AWU">
+</div>
+
+## Guide
+- [Setup Project](#setup)
+- [Accessing Functions](#accessing-functions)
+- [Uses of Functions and Their Results](#uses-of-functions-and-their-results)
+	- [Synchronous Functions](#synchronous-functions)
+	- [Asynchronous Functions](#asynchronous-functions)
+- [Project Informations](#authors)
+
+## Setup
+
+To download the package
+
+* Package Manager
+
+
+```bash
+  Install-Package OnePlatforms.AWU -Version 1.0.2
+```
+* .NET CLI
+
+```bash
+  dotnet add package OnePlatforms.AWU --version 1.0.2
+```
+
+* Package Reference
+```xml
+<PackageReference Include="OnePlatforms.AWU" Version="1.0.2" />
+```
+
+## Accessing Functions
+
+To access Win32 Methods, you need to import the package you downloaded.
+
+```CSharp
+using AdvancedWindowsUtility;
+```
+
+# Uses of Functions and Their Results
+
+## Synchronous Functions
+
+* Synchronous functions that handle operating system information.
+
+```CSharp
+using AdvancedWindowsUtility;
+
+/* 
+Function Name: GetAvailableMemorySpace
+Parameter Status: string (type) [Packet Type] [Types: Bytes, KBytes, MBytes, GBytes]
+Return Value: System.String
+Function Description: Returns the amount of memory available from the package type you specify.
+Thread Type: Synchronous [Sync]
+*/
+
+Windows.GetAvailableMemorySpace("MBytes"); // Example Result: 62424796
+
+
+/* 
+Function Name: IsRunningProcess
+Parameter Status: string (procParam) [Process Name]
+Return Value: System.String
+Function Description: Returns whether the specified application is running.
+Thread Type: Synchronous [Sync]
+*/
+
+Windows.IsRunningProcess("devenv.exe"); // Example Result: true
+
+
+/* 
+Function Name: GetRegisteredMail
+Parameter Status: Without parameters
+Return Value: System.String
+Function Description: Returns the Microsoft mail address registered to the operating system.
+Thread Type: Synchronous [Sync]
+*/
+
+Windows.GetRegisteredMail(); //Example Result: omerhuseyingul@outlook.com
+
+
+/* 
+Function Name: GetOperatingSystemName
+Parameter Status: Without parameters
+Return Value: System.String
+Function Description: Returns your operating system product name.
+Thread Type: Synchronous [Sync]
+*/
+
+Windows.GetOperatingSystemName(); // Example Result: Microsoft Windows 11 Pro
+
+
+/* 
+Function Name: GetUsername
+Parameter Status: Without parameters
+Return Value: System.String
+Function Description: Returns your operating system username.
+Thread Type: Synchronous [Sync]
+*/
+
+Windows.GetUsername(); // Example Result: OMERH
+```
+***
+
+## Asynchronous Functions
+
+* Asynchronous functions that handle operating system information.
+
+```CSharp
+using AdvancedWindowsUtility;
+
+/* 
+Function Name: GetAvailableMemorySpaceAsync
+Parameter Status: string (type) [Packet Type] [Types: Bytes, KBytes, MBytes, GBytes]
+Return Value: System.String
+Function Description: Returns the amount of memory available from the package type you specify.
+Thread Type: Asynchronous [Async]
+*/
+
+await Windows.GetAvailableMemorySpaceAsync("MBytes"); // Example Result: 62424796
+
+
+/* 
+Function Name: IsRunningProcess
+Parameter Status: string (procParam) [Process Name]
+Return Value: System.String
+Function Description: Returns whether the specified application is running.
+Thread Type: Asynchronous [Async]
+*/
+
+await Windows.IsRunningProcessAsync("devenv.exe"); // Example Result: true
+
+
+/* 
+Function Name: GetRegisteredMailAsync
+Parameter Status: Without parameters
+Return Value: System.String
+Function Description: Returns the Microsoft mail address registered to the operating system.
+Thread Type: Asynchronous [Async]
+*/
+
+await Windows.GetRegisteredMail(); //Example Result: omerhuseyingul@outlook.com
+
+
+/* 
+Function Name: GetOperatingSystemNameAsync
+Parameter Status: Without parameters
+Return Value: System.String
+Function Description: Returns your operating system product name.
+Thread Type: Asynchronous [Async]
+*/
+
+await Windows.GetOperatingSystemName(); // Example Result: Microsoft Windows 11 Pro
+
+
+/* 
+Function Name: GetUsernameAsync
+Parameter Status: Without parameters
+Return Value: System.String
+Function Description: Returns your operating system username.
+Thread Type: Asynchronous [Async]
+*/
+
+await Windows.GetUsername(); // Example Result: OMERH
+```
+
+
+
+
+## Authors
+
+- [@omerhuseyingul](https://www.github.com/omerhuseyingul)
+
+
+## Tech Stack
+
+**Programming Language:** C# (CSharp)
+
+**Framework:** .NET Core
+
+
+
+## Used By
+
+This project is used by the following companies.
+
+- qSoft Systems
+- One Platforms Open Source Software Unit
+- gamesense Entertainment
+- nuxonic Development
+
+
+## Support
+
+To contact the developer, see the communication channels below.
+- [Mail](mailto:omerhuseyingul@outlook.com)
+- [Discord](https://www.discord.com/users/852923308877545503)
+
+
+## License
+
+[MIT](https://github.com/One-Platforms/OnePlatforms.AWU/blob/main/LICENSE)
